c2bf0f309456ba769136c9b27f81c00ecb5f2d89
Hugo Vieira <hugoamvieira@users.noreply.github.com>
2022-10-19 13:33:54 +0100

    fixup! feat(worker): Report bytes up and down on Status updates

--- internal/session/service_worker_status_report.go
+++ internal/session/service_worker_status_report.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/boundary/internal/errors"
 	"github.com/hashicorp/boundary/internal/observability/event"
+	"github.com/hashicorp/go-multierror"
 )
 
 // StateReport is used to report on the state of a Session.
@@ -15,13 +16,15 @@ type StateReport struct {
 	Connections []Connection
 }
 
-// WorkerStatusReport is a domain service function that compares the state of
-// sessions and connections as reported by a Worker, to the known state in the
-// repositories. It returns a []StateReport for each session that is in the
-// canceling or terminated state. It also will check for any orphaned
-// connections, which is defined as a connection that is in an active state,
-// but was not reported by worker. Any orphaned connections will be marked as
-// closed.
+// WorkerStatusReport is a domain service function that, given a Worker's
+// session state reports, performs a few tasks:
+//  1. Updates the bytes up and down statistics for each reported connection.
+//  2. Compares the state of sessions and connections as reported by a Worker,
+//     to the known state in the repositories. It returns a StateReport object
+//     for each session that is in the canceling or terminated state.
+//  3. Checks for any orphaned connections, which is defined as a connection
+//     that is in an active state, but was not reported by worker. Any orphaned
+//     connections will be marked as closed.
 func WorkerStatusReport(ctx context.Context, repo *Repository, connRepo *ConnectionRepository, workerId string, report []StateReport) ([]StateReport, error) {
 	const op = "session.WorkerStatusReport"
 
@@ -36,23 +39,27 @@ func WorkerStatusReport(ctx context.Context, repo *Repository, connRepo *Connect
 		}
 	}
 
+	merr := new(multierror.Error)
+	err := connRepo.updateBytesUpBytesDown(ctx, reportedConnections...)
+	if err != nil {
+		merr = multierror.Append(merr, errors.New(ctx, errors.Internal, op, fmt.Sprintf("failed to update bytes up and down for worker reported connections: %v", err)))
+	}
+
 	notActive, err := repo.checkIfNoLongerActive(ctx, reportedSessions)
 	if err != nil {
-		return nil, errors.New(ctx, errors.Internal, op, fmt.Sprintf("Error checking session state for worker %s: %v", workerId, err))
+		merr = multierror.Append(merr, errors.New(ctx, errors.Internal, op, fmt.Sprintf("Error checking session state for worker %s: %v", workerId, err)))
 	}
 
 	closed, err := connRepo.closeOrphanedConnections(ctx, workerId, reportedConnectionIds)
 	if err != nil {
-		return notActive, errors.New(ctx, errors.Internal, op, fmt.Sprintf("Error closing orphaned connections for worker %s: %v", workerId, err))
+		merr = multierror.Append(merr, errors.New(ctx, errors.Internal, op, fmt.Sprintf("Error closing orphaned connections for worker %s: %v", workerId, err)))
 	}
 	if len(closed) > 0 {
 		event.WriteSysEvent(ctx, op, "marked unclaimed connections as closed", "controller_id", workerId, "count", len(closed))
 	}
 
-	err = connRepo.updateBytesUpBytesDown(ctx, reportedConnections...)
-	if err != nil {
-		return notActive, errors.New(ctx, errors.Internal, op, fmt.Sprintf("failed to update bytes up and down for worker reported connections: %v", err))
+	if merr.ErrorOrNil() != nil {
+		return nil, merr.ErrorOrNil()
 	}
-
 	return notActive, nil
 }
